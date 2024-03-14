a9c9e8d0c38fe343502576032c9300346b33de5f
Juan Sanchez <144753797+JsanchezArnedo03@users.noreply.github.com>
2024-03-13 19:31:32 -0500

    Adicion de nuevas clases,metodos y base de datos
    Clases: 
    ClienteCRUD
    VehiculoCRUD
    LoginCRUD
    Ingreso al sistema completo en la base de datos tienes las credenciales de acceso (llevan al mismo formulario)
    Falta tener mas información respecto a lo que debemos guardar de los clientes y los empleados

---
+++ VentaDeVehiculos-Desarrolador/VentaDeVehiculos-Desarrolador/comprayventa/src/entidades/Vehiculo.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+/**
+ *
+ * @author jssa3
+ */
+public class Vehiculo {
+    private int id;
+    private String marca;
+    private String modelo;
+    private int cantidadRuedas;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getCantidadRuedas() {
+        return cantidadRuedas;
+    }
+
+    public void setCantidadRuedas(int cantidadRuedas) {
+        this.cantidadRuedas = cantidadRuedas;
+    }
+}
