e0f29b8d4f8b10f1bb404384ad31128bc6e81d98
Juan Sanchez <144753797+JsanchezArnedo03@users.noreply.github.com>
2024-03-16 21:02:16 -0500

    Delete VentaDeVehiculos-Desarrolador directory

--- VentaDeVehiculos-Desarrolador/VentaDeVehiculos-Desarrolador/ProcedimientosDeAlmancenado.sql
+++
@@ -1,8 +0,0 @@
-/*PROCEDIMIENTOS DE ALMACENADO*/
-delimiter //
-create procedure login(Pusername nvarchar(500),Ppsw nvarchar(500))
-begin
-select l.username,l.psw,l.cargoFK,CONCAT(e.primerNombre," ",e.segundoNombre," ",e.primerApellido," ",e.segundoApellido)
-"empleado" from login l inner join empleado e on l.empleadoFK = e.idEmpleado where l.username = Pusername and l.psw =Ppsw;
-end;//
-call login("junior",10);
